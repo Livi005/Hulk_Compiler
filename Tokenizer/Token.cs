615c42f124ec033feb0af879ff8f22aa6b3db538
livi005 <ibaezmustelier@gmail.com>
2023-07-14 16:35:29 -0400

    Class token created

---
+++ Tokenizer/Token.cs
@@ -0,0 +1,19 @@
+﻿namespace Tokenizer;
+public enum TokenType
+{
+    plus,star,sub
+}
+
+public abstract class Token //Clase abstracta Token, crear token para cada tokentype
+{
+    TokenType type;
+    string value;
+
+    public Token(TokenType type,string value)
+    {
+        this.type = type;
+        this.value = value;
+    }
+}
+
+
